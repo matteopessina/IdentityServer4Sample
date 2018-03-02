51d58aa728b2f4d01b98bd2c97099ac637fefd9e
Pessina Matteo <matteo.pessina@reti.it>
2018-03-02 16:48:40 +0100

    minimal identity server setup

---
+++ IdentityServer4Sample/Config.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer4Sample
+{
+    internal class Config
+    {
+        internal static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        internal static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    //AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    
+
+                    // secret for authentication
+                    //ClientSecrets =
+                    //{
+                    //    new Secret("secret".Sha256())
+                    //},
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
