0672cb30fed32cbc9452b4df4631b955bd611a24
Pessina Matteo <matteo.pessina@reti.it>
2018-03-02 17:00:55 +0100

    set up resource server with identity

--- ResourceServer/Startup.cs
+++ ResourceServer/Startup.cs
@@ -23,7 +23,18 @@ namespace ResourceServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore()
+            .AddAuthorization()
+            .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "https://localhost:44354/";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ApiName = "api1";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +44,8 @@ namespace ResourceServer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
